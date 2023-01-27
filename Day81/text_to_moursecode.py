76876b6d269e6b3d715587b2e7b60b0a4a99df2e
Mirai003 <para-poom@hotmail.com>
2023-01-27 14:30:01 +0700

    Initial Commit

---
+++ Day81/text_to_moursecode.py
@@ -0,0 +1,31 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+encrypted_text = ""
+
+user_input = input("Put some text to convert to mourse code:").upper()
+# print(user_input)
+
+for letter in user_input:
+	if letter in MORSE_CODE_DICT:
+		encrypted_text += MORSE_CODE_DICT[letter]
+		encrypted_text += " "
+	else:
+		print("Error, Input must be alphabets or numbers.")
+		exit()
+
+print(encrypted_text)
